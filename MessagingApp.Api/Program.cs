ac2e9c17335b44c4f45fab0936bd55a7c4047850
Alan <alanscholar5@gmail.com>
2026-05-28 15:12:09 -0400

    Add active user repository as singleton

--- MessagingApp.Api/Program.cs
+++ MessagingApp.Api/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddSingleton<IChatMessageRepository, InMemoryRepository>();
+builder.Services.AddSingleton<IActiveUserRepository, ActiveUserRepository>();
 
 builder.Services.AddCors(options =>
 {
